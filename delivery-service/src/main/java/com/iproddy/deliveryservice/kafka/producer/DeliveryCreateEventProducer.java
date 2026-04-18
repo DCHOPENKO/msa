09e1d9d4a819b51b0f2b40434d033e8b96e47685
Dmitrii Chopenko <dchopenko1986@gmail.com>
2026-04-18 11:55:07 +0300

    lesson11: refactor to get idempotent key from header

--- delivery-service/src/main/java/com/iproddy/deliveryservice/kafka/producer/DeliveryCreateEventProducer.java
+++ delivery-service/src/main/java/com/iproddy/deliveryservice/kafka/producer/DeliveryCreateEventProducer.java
@@ -1,11 +1,15 @@
 package com.iproddy.deliveryservice.kafka.producer;
 
+import com.iproddy.common.util.constant.AsyncConstants;
 import com.iproddy.deliveryservice.config.properties.KafkaTopicProperties;
 import com.iproddy.deliveryservice.kafka.producer.dto.DeliveryCreateEvent;
 import com.iproddy.deliveryservice.mapper.DeliveryCreateEventMapper;
 import com.iproddy.deliveryservice.model.entity.Delivery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -22,7 +26,15 @@ public class DeliveryCreateEventProducer {
     public void send(Delivery delivery) {
         Objects.requireNonNull(delivery, "delivery must not be null");
         DeliveryCreateEvent event = deliveryCreateEventMapper.toEvent(delivery);
-        kafkaTemplate.send(kafkaTopicProperties.getDeliveryServiceCreatedEventTopic(), UUID.randomUUID().toString(), event);
+
+        Message<DeliveryCreateEvent> message = MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, kafkaTopicProperties.getDeliveryServiceCreatedEventTopic())
+                .setHeader(KafkaHeaders.KEY, String.valueOf(event.id()))
+                .setHeader(AsyncConstants.IDEMPOTENT_KEY_HEADER_NAME, UUID.randomUUID().toString())
+                .build();
+
+        kafkaTemplate.send(message);
     }
 
 }
--- order-service/src/main/java/com/iproddy/orderservice/kafla/consumer/IdempotentKafkaConsumer.java
+++ order-service/src/main/java/com/iproddy/orderservice/kafla/consumer/IdempotentKafkaConsumer.java
@@ -1,14 +1,17 @@
 package com.iproddy.orderservice.kafla.consumer;
 
+import com.iproddy.common.util.constant.AsyncConstants;
 import com.iproddy.orderservice.mapper.AsyncMessageMapper;
 import com.iproddy.orderservice.model.entity.AsyncMessage;
 import com.iproddy.orderservice.service.AsyncMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Header;
 import org.springframework.dao.DataIntegrityViolationException;
 
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -21,9 +24,16 @@ public abstract class IdempotentKafkaConsumer<T> {
     public void consume(ConsumerRecord<String, T> consumerRecord) {
         log.info("Received message: {} from topic {} partition {}, offset {}", consumerRecord.value(), consumerRecord.topic(), consumerRecord.partition(), consumerRecord.offset());
 
+        Header idempotentKeyHeader = consumerRecord.headers().lastHeader(AsyncConstants.IDEMPOTENT_KEY_HEADER_NAME);
+        if (idempotentKeyHeader == null) {
+            log.error("Idempotent key header is null for consumer record " + consumerRecord);
+            return;
+        }
+
         UUID idempotentKey;
         try {
-            idempotentKey = UUID.fromString(consumerRecord.key());
+            String rawKey = new String(idempotentKeyHeader.value(), StandardCharsets.UTF_8);
+            idempotentKey = UUID.fromString(rawKey);
         } catch (IllegalArgumentException e) {
             log.warn("Message with the wrong idempotent key: " + consumerRecord.key());
             return;
