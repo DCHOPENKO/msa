7d9d89d6d8cc93bc6c07ed5f51cd96c28e769f03
Dmitrii Chopenko <dchopenko1986@gmail.com>
2026-05-17 14:05:27 +0300

    lesson16: add discovery and load balancing

--- payment-service/src/main/java/com/iproddy/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/iproddy/paymentservice/PaymentServiceApplication.java
@@ -3,7 +3,9 @@ package com.iproddy.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class PaymentServiceApplication {
